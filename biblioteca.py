ac6d3db4fc371b94b6540cd79ffaca80bf89d841
x1ngue <az09.cecm@gmail.com>
2024-09-23 21:52:23 -0300

    adicionado arquivo oqfalta e arrumado loop quando erra a senha para conexao ao banco de dados

--- biblioteca.py
+++ biblioteca.py
@@ -1,17 +1,22 @@
 from pymongo import MongoClient
 import getpass
 from bson.objectid import ObjectId
+import sys
 
-senha = getpass.getpass("Digite a senha do MongoDB: ")
+while True:
+    senha = getpass.getpass("Digite a senha do MongoDB: ")
 
-uri = f"mongodb+srv://x1ngue:{senha}@cluster0.uwf0d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    uri = f"mongodb+srv://x1ngue:{senha}@cluster0.uwf0d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
-try:
-    client = MongoClient(uri)
-    db = client['biblioteca']
-    print("Conexão bem-sucedida ao MongoDB!")
-except Exception as e:
-    print("Erro ao conectar ao MongoDB:", e)
+    try:
+        client = MongoClient(uri)
+        db = client['biblioteca']
+        db_l = client.list_database_names()
+        print("Conexão bem-sucedida ao MongoDB!")
+        break
+    except Exception as e:
+        print("Erro ao conectar ao MongoDB:", e)
+        
 
 livros_collection = db['livros']
 usuarios_collection = db['usuarios']
